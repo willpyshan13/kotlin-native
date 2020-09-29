16968b2c384fc27d2ca1103fcc590add54680267
Ilya Matveev <ilya.matveev@jetbrains.com>
2020-09-29 16:52:29 +0700

    Revert "Vendoring gtest"
    This reverts commit bd7457d1189eee7961811fa10d59cb1f6ab4da53.

--- third_party/googletest/update_googletest.sh
+++ third_party/googletest/update_googletest.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-REVISION='10ade8473b698a8fe14ddb518c2abd228669657a'
-REPO='https://github.com/google/googletest.git'
-PREFIX='third_party/googletest/googletest'
 
-cd `git rev-parse --show-toplevel`
-echo "Updating googletest to ${REVISION}"
-git subtree pull --prefix="$PREFIX" "$REPO" "$REVISION" --squash
